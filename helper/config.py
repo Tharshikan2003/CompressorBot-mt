8e6d1abdb977e028a6051b6a60bcfbe93d3c513f
AMZ <80452444+amegh-amz@users.noreply.github.com>
2021-03-30 20:25:59 +0530

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -1,6 +1,5 @@
 #    This file is part of the Compressor distribution.
-#    Copyright (c) 2021 Danish_00
-#
+
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, version 3.
@@ -9,8 +8,7 @@
 #    WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 #    General Public License for more details.
-#
-#    License can be found in < https://github.com/1Danish-00/CompressorBot/blob/main/License> .
+
 
 from . import *
 
@@ -18,7 +16,7 @@ try:
     APP_ID = config("APP_ID", cast=int)
     API_HASH = config("API_HASH")
     BOT_TOKEN = config("BOT_TOKEN")
-    OWNER = config("OWNER_ID", default=1322549723, cast=int)
+    OWNER = config("OWNER_ID", default=1167010511, cast=int)
     LOG = config("LOG_CHANNEL", cast=int)
 except Exception as e:
     LOGS.info("Environment vars Missing")
