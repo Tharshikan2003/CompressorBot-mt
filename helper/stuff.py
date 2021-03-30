66334eca9f096e5be8a61cc5a434e1dd112d2bd4
AMZ <80452444+amegh-amz@users.noreply.github.com>
2021-03-30 22:26:30 +0530

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -32,8 +32,8 @@ async def start(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/CompressorBot"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("SOURCE CODE", url="t.me/movieTOwn007"),
+                Button.url("DEVELOPER", url="t.me/amzmtaccount"),
             ],
         ],
     )
@@ -59,8 +59,8 @@ async def beck(event):
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("SOURCE CODE", url="t.me/movieTOwn007"),
+                Button.url("DEVELOPER", url="t.me/amzmtaccount"),
             ],
         ],
     )
