862be0e5d1881eb3892a17f235ff35d585eeb1fa
AMZ <80452444+amegh-amz@users.noreply.github.com>
2021-03-30 20:29:06 +0530

    Update stuff.py

--- helper/stuff.py
+++ helper/stuff.py
@@ -1,6 +1,5 @@
 #    This file is part of the Compressor distribution.
-#    Copyright (c) 2021 Danish_00
-#
+
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, version 3.
@@ -10,7 +9,7 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 #    General Public License for more details.
 #
-#    License can be found in < https://github.com/1Danish-00/CompressorBot/blob/main/License> .
+
 
 from .worker import *
 
