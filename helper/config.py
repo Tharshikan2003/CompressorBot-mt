064cf1a2067a086c652480a4fc59fe0df8e2998e
AMZ <80452444+amegh-amz@users.noreply.github.com>
2021-03-30 23:11:33 +0530

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -17,8 +17,7 @@ try:
     API_HASH = config("API_HASH")
     BOT_TOKEN = config("BOT_TOKEN")
     OWNER = config("OWNER_ID", default=1167010511, cast=int)
-  
-  LOG = config("LOG_CHANNEL", cast=int)
+    LOG = config("LOG_CHANNEL", cast=int)
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
